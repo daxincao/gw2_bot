5a561b63bca50e1e6af073b0ba1a301cb8a72923
Xin Cao <xincao@uchicago.edu>
2015-02-11 16:03:57 -0500

    Threads now write directly into db

--- api_request_threading.py
+++ api_request_threading.py
@@ -10,12 +10,20 @@ import threading
 import logging
 import time
 import sample_list as sl # 1233 test items
+import psycopg2	
+import psycopg2_extension as ps2
 		
 
-def worker(batch):
+def worker(batch, **kwargs):
 	logger.info('Starting')
+	
+	# Query API
 	return_batch = al.get_listing_dataframe(batch)
 	logger.debug(return_batch)
+	
+	# Write result to db
+	write_batch_to_db(con=con, batch=return_batch)
+	
 	logger.info("%s threads remaining" % threading.active_count())
 	logger.info('Exiting')		
 
@@ -27,8 +35,13 @@ def batch_maker(large_list, n):
 		batch = large_list[:n]
 		yield batch
 		large_list = large_list[n:] 			
-			
 
+def write_batch_to_db(con, batch):
+	# Write batch into db
+	ps2.write_frame(batch, 'new_table', con, flavor='postgresql', if_exists='append')
+	logger.info("Batch Completed!")
+	
+				
 if __name__ == '__main__':
 	
 	logging.basicConfig(level=logging.INFO,
@@ -37,12 +50,15 @@ if __name__ == '__main__':
 				
 	logger = logging.getLogger(__name__)
 	
+	# Initialize connection to db
+	con = psycopg2.connect("dbname='gw2_db' user='postgres'")
+	
 	# Generate batches of 200
 	batches = batch_maker(sl.sample_list, 200)
 	
 	# Start threading
 	for batch in batches:
-		w = threading.Thread(target=worker, args=(batch,))
+		w = threading.Thread(target=worker, args=(batch,), kwargs={'con':con})
 		w.start()
 
 
